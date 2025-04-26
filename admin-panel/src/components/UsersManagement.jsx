65be70944fd72eff494c67327a96e33bb652cb8d
Manu Gangwar <manugangwar8650@gmail.com>
2025-04-26 14:09:09 +0530

    Update UsersManagement.jsx

--- admin-panel/src/components/UsersManagement.jsx
+++ admin-panel/src/components/UsersManagement.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
+import axios from "axios";
 import 'react-toastify/dist/ReactToastify.css';
 
 import "../styles/usersManagement.css";
