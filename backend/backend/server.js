676d05654e44e0c05201d5dee66ebb5b84fd5e51
Manu Gangwar <manugangwar8650@gmail.com>
2025-04-26 12:28:21 +0530

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -36,9 +36,9 @@ app.use('/api/transactions', transactionRoutes); // ✅ Add this line
 app.use('/api/adminAuth', adminAuth); // ✅ Add this line
 app.use("/api/referral",referral);
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 
