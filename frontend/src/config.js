953cc8babd3d9d0da68cf2521a5a412839eb5708
Manu Gangwar <manugangwar8650@gmail.com>
2025-04-26 22:20:42 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,9 @@
-export const BASE_URL = "https://task-b1w0.onrender.com"
+// export const BASE_URL = "https://task-b1w0.onrender.com"
 // export const BASE_URL = "172.20.10.3:5000"
 // export const BASE_URL = "172.20.10.3:5000"
+
+// config.js
+export const BASE_URL = window.location.hostname.includes("localhost")
+  ? "http://localhost:5000"
+  : "https://task-b1w0.onrender.com";
+
