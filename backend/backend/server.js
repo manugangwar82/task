c647e3d97193bea44cfb70283dad2ec1ee3146a3
Manu Gangwar <manugangwar8650@gmail.com>
2025-04-28 16:29:28 +0530

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 const corsOptions = {
- origin: ["earnwithcampa.store", "https://admin-9fyk.onrender.com"], // ✅ Array of allowed domains
+origin: ["https://earnwithcampa.store", "https://admin-9fyk.onrender.com"], // ✅ Array of allowed domains
   credentials: true, // ✅ Important for cookies / Authorization header
 };
 
